c546185d2dd5d03f424bb4487642b3e289b9add0
kps-user <niamh.p.bates@gmail.com>
2016-11-16 14:15:13 +1300

    Update module6.py

--- module6.py
+++ module6.py
@@ -20,26 +20,26 @@ GPIO.setup(15, GPIO.OUT)
 
 interval = 1
 
-print 'forward :)'
+print 'forward'
 GPIO.output(11, True)
 GPIO.output(13, True)
 time.sleep(interval)
 GPIO.output(11, False)
 GPIO.output(13, False)
    
-print 'back [_(^3^)_]'
+print 'back'
 GPIO.output(7, True)
 GPIO.output(15, True)
 time.sleep(interval)
 GPIO.output(7, False)
 GPIO.output(15, False)
 
-print 'left :)'
+print 'left'
 GPIO.output(13, True)
 time.sleep(interval)
 GPIO.output(13, False)
       
-print 'right :)'
+print 'right'
 GPIO.output(11, True)
 time.sleep(interval)
 GPIO.output(11, False)
